e60097015653a06780d48b42104a608c27a2eed8
Alice Brunel <alc.brunel@gmail.com>
2019-07-23 21:34:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,7 +47,7 @@ class TicTacToe
     index = input_to_index(input)
     if valid_move?(index) == true
       move(index)
-      display_board
+      display_boardz
     else
       puts "This move is not correct."
       turn
