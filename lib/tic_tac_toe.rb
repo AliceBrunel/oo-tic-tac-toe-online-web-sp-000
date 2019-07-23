0cc8c44748e90437b0cb13d7f6a63fee7bffd5f2
Alice Brunel <alc.brunel@gmail.com>
2019-07-23 21:25:18 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,5 +28,16 @@ class TicTacToe
     user_input.to_i - 1
   end
 
+   def move(board, index, current_player)
+     board[index] = current_player
+   end 
+    
+   def position_taken?(board, index)
+     board[index] != " "
+   end
+    
+   def valid_move?(board, index)
+     index.between?(0,8) && !position_taken?(board, index)
+   end
 
 end
